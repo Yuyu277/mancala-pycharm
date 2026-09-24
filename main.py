a65e05af3633880b2f8347247f548b645938a5c9
Yuyu277 <flyingmonkeyYAY@gmail.com>
2026-09-24 14:51:39 -0400

    finished move_stones

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ def print_board(board):
     print("-"*35)
     rowB = ("     ".join(str(x) for x in board[8:14]))[::-1]
     print("B: " + rowB)
-    print(str(board[0]) + "                                  " + str(board[7]))
+    print(str(" " + str(board[0])) + "                                  " + str(board[7]))
     rowA = "     ".join(str(x) for x in board[1:7])
     print("A: " + rowA)
     print("-"*35)
     print("   1     2     3     4     5     6")
 
 def is_valid_move(move_num):
-    if(move_num>=1 and move_num<=6 and playerATurn or move_num>=8 and move_num<=13 and not playerATurn):
-        if(board[move_num] > 0):
+    if 1 <= move_num <= 6 and playerATurn or 8 <= move_num <= 13 and not playerATurn:
+        if board[move_num] > 0:
             return True
         else:
             return False
@@ -23,22 +23,37 @@ def is_valid_move(move_num):
 def move_stones(move_num):
     move_amt = board[move_num]
     board[move_num] = 0
-    for i in range(move_amt):
-        board_on = (move_num + 1 + i)/14
+    i = 0
+    while move_amt > 0:
+        board_on = (move_num + 1 + i) % 14
         if playerATurn and board_on == 0 or not playerATurn and board_on == 7:
-            #left off here
             i += 1
-            board_on = (move_num + 1 + i) / 14
+            board_on = (move_num + 1 + i) % 14
         board[board_on] += 1
+        i += 1
+        move_amt -= 1
+    #steal
+    if board[(move_num + i) % 14] - 1 == 0 and not board[(14 - move_num - i) % 14] == 0 and not (14 - move_num - i) % 14 == 7 and not (14 - move_num - i) % 14 == 0:
+        if playerATurn and (move_num + 1) % 14 < 7 or not playerATurn and (move_num + 1) % 14 > 7:
+            print("Steal!!!")
+            if playerATurn:
+                board[7] += board[(move_num + i) % 14] + board[(14 - move_num - i) % 14]
+            else:
+                board[0] += board[(move_num + i) % 14] + board[(14 - move_num - i) % 14]
+            board[(move_num + i) % 14] = 0
+            board[(14 - move_num - i) % 14] = 0
+    if playerATurn and (move_num + i) % 14 == 7 or not playerATurn and (move_num + i) % 14 == 0:
+        print_board(board)
+        take_turn()
     print_board(board)
 
 
 def take_turn():
     if playerATurn:
-        print("Player A's turn: ")
+        print("Player A's turn! ")
         print("Possible moves: 1, 2, 3, 4, 5, 6")
     else:
-        print("Player B's turn: ")
+        print("Player B's turn! ")
         print("Possible moves: 8, 9, 10, 11, 12, 13")
     users_move = int(input("Your move: "))
     while True:
@@ -49,7 +64,7 @@ def take_turn():
             print("Invalid move! Try again.")
             users_move = int(input("Your move: "))
 
-#running stuff
+#running
 playerATurn = True
 print_board(board)
 while True:
